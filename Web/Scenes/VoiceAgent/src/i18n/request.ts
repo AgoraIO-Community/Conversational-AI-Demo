53be8426d4cebfcc3550fd04f67e8fc5456bf762
czhen <56986964+shczhen@users.noreply.github.com>
2025-03-03 18:11:06 +0800

    chore: update default locale

--- Web/Scenes/VoiceAgent/src/i18n/request.ts
+++ Web/Scenes/VoiceAgent/src/i18n/request.ts
@@ -3,8 +3,7 @@ import { getRequestConfig } from 'next-intl/server'
 export default getRequestConfig(async () => {
   // Provide a static locale, fetch a user setting,
   // read from `cookies()`, `headers()`, etc.
-  // TODO: update to en-US by default
-  const locale = process.env.NEXT_PUBLIC_LOCALE || 'zh-CN'
+  const locale = process.env.NEXT_PUBLIC_LOCALE || 'en-US'
 
   return {
     locale,
