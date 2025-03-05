bb8ac3c81aa314100c4a7d8bab3bcdbb6e8fe5d2
czhen <56986964+shczhen@users.noreply.github.com>
2025-03-05 11:02:57 +0800

    chore: update code (#24)
    remove useless code

--- Web/Scenes/VoiceAgent/src/app/api/sso/login/route.ts
+++ Web/Scenes/VoiceAgent/src/app/api/sso/login/route.ts
@@ -17,7 +17,7 @@ export async function GET(request: NextRequest) {
 
   const url = `${agentServer}${REMOTE_SSO_LOGIN}`
 
-  // 校验参数
+  // validate params
   const queryParams = querySchema.safeParse(
     Object.fromEntries(request.nextUrl.searchParams)
   )
