b9ece31c5734ef0bcdc296bda1a0d3d8631bf5ea
HeZhengQing <hezhengqing@agora.io>
2025-04-03 15:45:46 +0800

    [FEAT] 1.3.0 update

--- iOS/Scenes/ConvoAI/ConvoAI/ConvoAI/Classes/Main/Chat/ChatViewController.swift
+++ iOS/Scenes/ConvoAI/ConvoAI/ConvoAI/Classes/Main/Chat/ChatViewController.swift
@@ -512,7 +512,11 @@ extension ChatViewController {
             return
         }
         manager.updateAgentState(.disconnected)
-        channelName = RtcEnum.getChannel()
+        if DeveloperModeViewController.getDeveloperMode() {
+            channelName = "agent_debug_\(UUID().uuidString.prefix(8))"
+        } else {
+            channelName = "agent_\(UUID().uuidString.prefix(8))"
+        }
         agentUid = AppContext.agentUid
         remoteIsJoined = false
         
