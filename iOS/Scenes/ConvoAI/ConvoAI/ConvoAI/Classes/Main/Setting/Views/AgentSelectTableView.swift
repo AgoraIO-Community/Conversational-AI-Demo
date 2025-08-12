eb63130c73defc68e02a4fd64ea1a3d698f6a9a3
qinhui <>
2025-08-12 18:16:08 +0800

    bug fix

--- iOS/Scenes/ConvoAI/ConvoAI/ConvoAI/Classes/Main/Setting/Views/AgentSelectTableView.swift
+++ iOS/Scenes/ConvoAI/ConvoAI/ConvoAI/Classes/Main/Setting/Views/AgentSelectTableView.swift
@@ -82,14 +82,6 @@ extension AgentSelectTableView: UITableViewDataSource, UITableViewDelegate {
         cell.bottomLine.isHidden = (indexPath.row == dataSource.count - 1)
         return cell
     }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return max(UITableView.automaticDimension, 56)
-    }
-
-    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 56
-    }
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
