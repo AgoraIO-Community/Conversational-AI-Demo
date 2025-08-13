afc4c53d9cd208b6c47947aa149bbf47b9d2f6cf
zhangwei <zhangwei03@shengwang.cn>
2025-08-13 20:53:34 +0800

    [Android]Update convoai api doc.

--- Android/scenes/convoai/src/main/java/io/agora/scene/convoai/convoaiApi/IConversationalAIAPI.kt
+++ Android/scenes/convoai/src/main/java/io/agora/scene/convoai/convoaiApi/IConversationalAIAPI.kt
@@ -380,6 +380,8 @@ enum class MessageType(val value: String) {
 }
 
 /**
+ * @technical preview
+ *
  * Voiceprint status enumeration
  * Used to track the status of voiceprint registration and sending
  * Helps in managing voiceprint lifecycle and UI display by identifying different states
@@ -419,6 +421,8 @@ enum class VoiceprintStatus(val value: String) {
 }
 
 /**
+ * @technical preview
+ *
  * @property timeOffset For example, from unregister to register success, how much time it takes
  * @property timestamp Event occurrence timestamp (milliseconds since epoch, i.e., since January 1, 1970 UTC)
  * @property status Voice print status
