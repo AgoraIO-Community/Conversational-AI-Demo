f2a3ee590db91a590ab16b5e186f50f1c7715b83
HeZhengQing <hezhengqing@agora.io>
2025-09-19 00:48:55 +0800

    [FIX] bug fix ios

--- iOS/Scenes/ConvoAI/ConvoAI/ConvoAI/Classes/Main/Setting/VC/AgentSettingViewController.swift
+++ iOS/Scenes/ConvoAI/ConvoAI/ConvoAI/Classes/Main/Setting/VC/AgentSettingViewController.swift
@@ -367,6 +367,10 @@ extension AgentSettingViewController: AgentStateDelegate {
     func stateManager(_ manager: AgentStateManager, userIdDidUpdated userId: String) {
         channelInfoView.updateUserId(userId)
     }
+    
+    func stateManager(_ manager: AgentStateManager, voiceprintDidUpdated enabled: Bool) {
+        channelInfoView.updateVoiceprintState()
+    }
 }
 
 extension AgentSettingViewController: AgentSettingDelegate {
@@ -386,6 +390,7 @@ extension AgentSettingViewController: AgentSettingDelegate {
     
     func settingManager(_ manager: AgentSettingManager, aiVadStateDidUpdated state: Bool) {
         agentSettingsView.updateAiVadState(state)
+        channelInfoView.updateAiVadState()
     }
     
     func settingManager(_ manager: AgentSettingManager, transcriptModeDidUpdated mode: TranscriptDisplayMode) {
