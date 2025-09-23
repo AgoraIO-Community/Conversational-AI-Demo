ccfcc1b748d01367914943696efb3af28ef39a26
zhangwei <zhangwei03@shengwang.cn>
2025-09-23 11:16:18 +0800

    [Android]Remove user profile ui.

--- Android/scenes/convoai/src/main/java/io/agora/scene/convoai/ui/mine/CovMineFragment.kt
+++ Android/scenes/convoai/src/main/java/io/agora/scene/convoai/ui/mine/CovMineFragment.kt
@@ -1,15 +1,10 @@
 package io.agora.scene.convoai.ui.mine
 
-import android.graphics.Color
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.core.view.isVisible
-import io.agora.scene.common.R
-import io.agora.scene.common.constant.SSOUserManager
 import io.agora.scene.common.debugMode.DebugConfigSettings
 import io.agora.scene.common.ui.BaseFragment
-import io.agora.scene.common.util.toast.ToastUtil
 import io.agora.scene.convoai.databinding.CovFragmentMineBinding
 import io.agora.scene.convoai.iot.manager.CovIotDeviceManager
 import io.agora.scene.convoai.iot.ui.CovIotDeviceListActivity
@@ -48,31 +43,12 @@ class CovMineFragment : BaseFragment<CovFragmentMineBinding>() {
         Log.d("UserViewModel", "UserViewModel:$userViewModel $this")
         setupClickListeners()
         updateDeviceCount()
-        updateUserInfoFromManager()
     }
 
     private fun setupClickListeners() {
         mBinding?.apply {
-            layoutPersona.setOnClickListener {
+            viewMineTop.setOnClickListener {
                 DebugConfigSettings.checkClickDebug()
-            }
-            tvNickname.setOnClickListener {
-                val activity = activity ?: return@setOnClickListener
-                CovProfileNicknameActivity.startActivity(activity)
-            }
-
-            tvSelectAddress.setOnClickListener {
-                val activity = activity ?: return@setOnClickListener
-                CovProfileGenderActivity.startActivity(activity)
-            }
-
-            tvSelectBirthday.setOnClickListener {
-                showCustomBirthdayPicker()
-            }
-
-            tvIntroduction.setOnClickListener {
-                val activity = activity ?: return@setOnClickListener
-                CovProfileAboutMeActivity.startActivity(activity)
             }
 
             flDeviceContent.setOnClickListener {
@@ -91,68 +67,9 @@ class CovMineFragment : BaseFragment<CovFragmentMineBinding>() {
         }
     }
 
-    private fun updateUserInfo(userInfo: io.agora.scene.common.net.SSOUserInfo) {
-        mBinding?.apply {
-            // Update nickname
-            tvNickname.text = userInfo.nickname.ifEmpty {
-                userInfo.displayName.ifEmpty {
-                    getString(io.agora.scene.convoai.R.string.cov_mine_not_set)
-                }
-            }
-
-            // Update birthday
-            tvSelectBirthday.text =
-                userInfo.birthday.ifEmpty { getString(io.agora.scene.convoai.R.string.cov_mine_select) }
-
-            // Update bio/introduction
-            tvIntroduction.text =
-                userInfo.bio.ifEmpty { getString(io.agora.scene.convoai.R.string.cov_mine_about_me_hint) }
-
-            // Update gender display
-            when (userInfo.gender.lowercase()) {
-                "male" -> {
-                    ivUserAvatar.setImageResource(R.drawable.common_default_male)
-                    ivUserAvatar.setBackgroundResource(R.drawable.app_bg_mine_avatar)
-                    tvSelectAddress.text = getString(io.agora.scene.convoai.R.string.cov_mine_mr)
-                    viewGotoCreate.isVisible = false
-                    viewAvatar.isVisible = true
-                    viewAvatar.setBackgroundResource(R.drawable.app_avatar_boy_en)
-                }
-
-                "female" -> {
-                    ivUserAvatar.setImageResource(R.drawable.common_default_female)
-                    ivUserAvatar.setBackgroundResource(R.drawable.app_bg_mine_avatar)
-                    tvSelectAddress.text = getString(io.agora.scene.convoai.R.string.cov_mine_ms)
-                    viewGotoCreate.isVisible = false
-                    viewAvatar.isVisible = true
-                    viewAvatar.setBackgroundResource(R.drawable.app_avatar_gril_en)
-                }
-
-                else -> {
-                    ivUserAvatar.setImageResource(R.drawable.common_default_user_avatar)
-                    ivUserAvatar.setBackgroundColor(Color.TRANSPARENT)
-                    tvSelectAddress.text = getString(io.agora.scene.convoai.R.string.cov_mine_select)
-                    viewGotoCreate.isVisible = true
-                    viewAvatar.isVisible = false
-                }
-            }
-        }
-    }
-
     override fun onResume() {
         super.onResume()
         updateDeviceCount()
-        // Update user info when returning from other activities
-        updateUserInfoFromManager()
-    }
-
-    private fun updateUserInfoFromManager() {
-        // Since user can access this fragment, they must be logged in
-        val userInfo = SSOUserManager.userInfo
-        if (userInfo != null) {
-            updateUserInfo(userInfo)
-        }
-        // If userInfo is null, it might be loading from local storage, will be updated when available
     }
 
     private fun updateDeviceCount() {
@@ -162,32 +79,5 @@ class CovMineFragment : BaseFragment<CovFragmentMineBinding>() {
         } else {
             mBinding?.tvDeviceCount?.text = getString(io.agora.scene.convoai.R.string.cov_mine_no_devices)
         }
-    }
-
-    /**
-     * Show custom birthday picker using third-party calendar library
-     */
-    private fun showCustomBirthdayPicker() {
-        val currentBirthday = mBinding?.tvSelectBirthday?.text?.toString() ?: ""
-
-        val dialog = CovBirthdayPickerDialog.newInstance(
-            selectedDate = currentBirthday.takeIf { it.isNotEmpty() }
-        ) { selectedDate ->
-            // Update UI
-            mBinding?.tvSelectBirthday?.text = selectedDate
-
-            // Update user info via API
-            userViewModel.updateUserInfo(
-                birthday = selectedDate
-            ) { result ->
-                result.onSuccess {
-                    ToastUtil.show(io.agora.scene.convoai.R.string.cov_settings_update_success)
-                }.onFailure { exception ->
-                    ToastUtil.show("${getString(io.agora.scene.convoai.R.string.cov_settings_update_failed)} ${exception.message}")
-                }
-            }
-        }
-
-        dialog.show(childFragmentManager, "CustomBirthdayPicker")
     }
 }
